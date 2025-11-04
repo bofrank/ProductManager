4f84187246a6cc9563e01241ceb71f7915317fc8
bofrank <bo@bofrank.com>
2025-11-04 19:32:35 +0900

    Adding functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import AddProject from "./components/AddProject";
+
 function App() {
   return (
     <>
@@ -5,7 +7,7 @@ function App() {
       <main className="h-screen my-8 flex gap-8">
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
           YOUR PROJECTS
-          <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add Project</button>
+          <AddProject />
         </aside>
       </main>
     </>
