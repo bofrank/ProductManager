f1c3f07b1ef83d066986cb5d58fc9bf02ce2eb8f
bofrank <bo@bofrank.com>
2025-11-04 19:33:47 +0900

    Adding return for component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AddProject from "./components/AddProject";
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <h1 className="my-8 text-center text-5xl font-bold">Product Manager</h1>
       <main className="h-screen my-8 flex gap-8">
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
           YOUR PROJECTS
