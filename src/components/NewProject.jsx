ae1fb64d7586ae62a60fa897f97177b9b5153dad
bofrank <bo@bofrank.com>
2025-11-04 21:26:41 +0900

    Tailwind styling.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -3,15 +3,15 @@ import Input from './Input.jsx';
 
 export default function NewProject({visibility}){
     return (
-        <div className={visibility}>
+        <div className="w-[35rem] mt-16 {{visibility}}">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="text-stone-800 hover:text-stone-950">Save</button></li>
+                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div className="w-[35rem] mt-16">
-                <Input label="Title" className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+                <Input label="Title" />
                 <Input label="Description" textarea />
-                <Input label="Due Date" className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" />
+                <Input label="Due Date" />
             </div>    
         </div>
     )
