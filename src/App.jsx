e9b98b4a8faab8079d83eea725739cce67b777e8
bofrank <bo@bofrank.com>
2025-11-04 20:34:38 +0900

    Adjusting sidebar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import AddProject from "./components/AddProject";
 import ProjectForm from "./components/ProjectForm";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Product Manager</h1>
       <main className="h-screen my-8 flex gap-8">
         <ProjectsSidebar />
-        <main className="h-screen my-8 flex gap-8">
+        <div>
           <p className="flex flex-col gap-1 my-4">
             No Project Selected
           </p>
@@ -18,7 +16,7 @@ function App() {
           <button className="text-stone-800 hover:text-stone-950">
             Create new project
           </button>
-        </main>
+        </div>
       </main>
       <div>
         <ProjectForm visibility="invisible" />
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -3,8 +3,11 @@ import AddProject from "./AddProject";
 export default function ProjectsSidebar(){
     return (
         <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-            YOUR PROJECTS
-            <AddProject />
+            <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">YOUR PROJECTS</h2>
+            <div>
+                <AddProject />
+            </div>
+            <ul className="mt-8">...</ul>
         </aside>
     )
 }
