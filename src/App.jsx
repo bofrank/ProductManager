80a377255ce3a464bf35f25453798a118df2611b
bofrank <bo@bofrank.com>
2025-11-04 19:13:40 +0900

    adding styles and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ function App() {
   return (
     <>
       <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <main className="h-screen my-8 flex gap-8">
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+          YOUR PROJECTS
+          <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">+ Add Project</button>
+        </aside>
+      </main>
     </>
   );
 }
